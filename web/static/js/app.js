aaf5d31469fd8ff4bac445debe263bf864306a39
Nikko Miu <nikko.miu@nikkomiu.com>
2016-07-12 13:19:01 -0500

    Added jQuery to vendor folder :disappointed:

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,6 +4,8 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import "bootstrap-sass"
+
 // Import local files
 //
 // Local files can be imported directly using relative
